43062169919d0c80642c644523f05d9311781a40
rangarajuakhil <rangarajuakhil@outlook.com>
2019-02-03 22:23:57 -0600

    Implemented Delete Controller

---
+++ src/main/java/com/xmltopdf/controller/DeleteController.java
@@ -0,0 +1,20 @@
+package com.xmltopdf.controller;
+
+import com.xmltopdf.service.DeleteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeleteController {
+
+    @Autowired
+    private DeleteService deleteService;
+
+    @DeleteMapping
+    public String deleteFiles(@RequestParam("path") String path){
+        deleteService.deleteHtmlFiles(path);
+    }
+
+}
--- src/main/java/com/xmltopdf/controller/XMLToPDFController.java
+++ src/main/java/com/xmltopdf/controller/XMLToPDFController.java
@@ -1,15 +1,13 @@
 package com.xmltopdf.controller;
 
 import com.xmltopdf.response.ConversionResponse;
+import com.xmltopdf.service.DeleteService;
 import com.xmltopdf.service.XMLToPDFService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
 
 @RestController
 public class XMLToPDFController {
@@ -17,8 +15,14 @@ public class XMLToPDFController {
     @Autowired
     private XMLToPDFService xmlToPDFService;
 
+    @Autowired
+    private DeleteService deleteService;
+
     @PostMapping("/convert")
     public ResponseEntity<ConversionResponse> convertToPdf(@RequestParam("path") String folderPath){
-        return xmlToPDFService.convertToPDF(folderPath);
+        ResponseEntity<ConversionResponse> response = xmlToPDFService.convertToPDF(folderPath);
+        String deleteResponse = deleteService.deleteHtmlFiles(folderPath);
+        System.out.println(deleteResponse);
+        return response;
     }
 }
